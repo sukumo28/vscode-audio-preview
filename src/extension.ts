cdaf895bf1359150337c07bc5f7108f3f6d8cdb4
sukumo28 <59384202+sukumo28@users.noreply.github.com>
2024-03-10 16:03:13 +0900

    add log

--- src/extension.ts
+++ src/extension.ts
@@ -2,5 +2,10 @@ import * as vscode from 'vscode';
 import { AudioPreviewEditorProvider } from "./audioPreviewEditor";
 
 export function activate(context: vscode.ExtensionContext) {
+	const extension = vscode.extensions.getExtension('sukumo28.wav-preview');
+	console.log("version", extension?.packageJSON?.version);
+	console.log("extension.extensionKind", extension?.extensionKind);
+	console.log("vscode.env.uiKind", vscode.env.uiKind);
+	console.log("vscode.env.remoteName", vscode.env.remoteName);
 	context.subscriptions.push(AudioPreviewEditorProvider.register(context));
 }
